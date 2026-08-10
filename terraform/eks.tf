c39d7dfed7f7e77cb4789fd1d968544b640232f7
Agha Fardeen <131296354+AghaFardeen123@users.noreply.github.com>
2026-08-11 02:59:11 +0500

    Create eks.tf

---
+++ terraform/eks.tf
@@ -0,0 +1,37 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.24"
+
+  cluster_name    = var.cluster_name
+  cluster_version = var.cluster_version
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  cluster_endpoint_public_access = true
+
+  cluster_endpoint_public_access_cidrs = [var.admin_cidr]
+
+  cluster_enabled_log_types = ["api", "audit", "authenticator", "controllerManager", "scheduler"]
+
+  enable_cluster_creator_admin_permissions = true
+
+  eks_managed_node_group_defaults = {
+    ami_type = "AL2023_x86_64_STANDARD"
+  }
+
+  eks_managed_node_groups = {
+    default = {
+      instance_types = var.node_instance_types
+      min_size       = var.node_min_size
+      max_size       = var.node_max_size
+      desired_size   = var.node_desired_size
+
+      labels = {
+        role = "general"
+      }
+    }
+  }
+
+  tags = local.common_tags
+}
