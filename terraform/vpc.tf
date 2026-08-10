d9a393ceda512b39ffbbc6e53b8b34c1eaf34a69
Agha Fardeen <131296354+AghaFardeen123@users.noreply.github.com>
2026-08-11 02:36:17 +0500

    Create vpc.tf

---
+++ terraform/vpc.tf
@@ -0,0 +1,20 @@
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.8"
+
+  name = "${var.cluster_name}-vpc"
+  cidr = var.vpc_cidr
+
+  azs             = local.azs
+  private_subnets = [for k, v in local.azs : cidrsubnet(var.vpc_cidr, 4, k)]
+  public_subnets  = [for k, v in local.azs : cidrsubnet(var.vpc_cidr, 4, k + 8)]
+
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+
+  public_subnet_tags  = merge(local.eks_shared_tags, { "kubernetes.io/role/elb" = 1 })
+  private_subnet_tags = merge(local.eks_shared_tags, { "kubernetes.io/role/internal-elb" = 1 })
+
+  tags = local.common_tags
+}
